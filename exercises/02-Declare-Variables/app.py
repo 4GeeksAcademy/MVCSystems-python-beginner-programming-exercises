a4d527cfb9680c7d6711c47a6bc13a567a7cae60
MVCSystems <carlosmerav@gmail.com>
2024-08-23 19:13:03 -0500

    Add

--- exercises/02-Declare-Variables/app.py
+++ exercises/02-Declare-Variables/app.py
@@ -1,1 +1,8 @@
 # ✅ ↓ your code here ↓ ✅
+
+name = "John"
+age = 25
+height = 1.80
+is_married = False
+print(name, age, height, is_married)
+print(type(name))
--- exercises/03-Print-Variables-In-The-Console/app.py
+++ exercises/03-Print-Variables-In-The-Console/app.py
@@ -1,1 +1,6 @@
 # ✅ ↓ your code here ↓ ✅
+
+
+name = "John"
+age = 25
+print(name, age)
